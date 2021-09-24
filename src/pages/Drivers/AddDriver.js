9fe7a1bb48baa478b1a4399c3e60afa6bf50980a
Ade Hermawan <app@adehermawan.com>
2021-09-24 15:46:01 +0700

    add requerd field and phone number notif

--- src/pages/Drivers/AddDriver.js
+++ src/pages/Drivers/AddDriver.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import { Form, Button, Container } from 'react-bootstrap'
+import { Form, Button, Container, Alert } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { addNewDriver } from '../../store/action/transporter'
@@ -13,6 +13,8 @@ class AddDriver extends Component {
             phoneNumber: '',
             idCard: '',
             driverLicense: '',
+            showAlert:false,
+            errorMsg:null,
         }
 
     }
@@ -20,12 +22,19 @@ class AddDriver extends Component {
     submitData = (e) => {
         e.preventDefault();
         const { driverName, phoneNumber, idCard, driverLicense } = this.state
-        this.props.addNewDriver(driverName, phoneNumber, idCard, driverLicense)
+        if (driverName === '' || phoneNumber == '') {
+            this.setState({errorMsg:'Driver name and Phone number are required'});
+        }else{
+            this.props.addNewDriver(driverName, phoneNumber, idCard, driverLicense)
+        }
         
     }
 
     changeText = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
+        this.setState({ [event.target.name]: event.target.value });
+        if(event.target.name == 'phoneNumber'){
+            this.setState({showAlert:true});
+        }
     }
 
     onFileChange = event => { 
@@ -39,13 +48,15 @@ class AddDriver extends Component {
             <React.Fragment>
                 <Container>
                     <Form>
+                        {this.state.errorMsg && <><small style={{ color: 'red' }}>{this.state.errorMsg}</small><br /></>}<br />
                         <Form.Group>
                             <Form.Label>Driver Name</Form.Label>
-                            <Form.Control name="driverName" onChange={(event) => this.changeText(event)} type="text" placeholder="" />
+                            <Form.Control required name="driverName" onChange={(event) => this.changeText(event)} type="text" placeholder="" />
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Phone Number</Form.Label>
-                            <Form.Control name="phoneNumber" onChange={(event) => this.changeText(event)} type="text" placeholder="" />
+                            <Form.Control required name="phoneNumber" onChange={(event) => this.changeText(event)} type="text" placeholder="" /><br />
+                            <Alert show={this.state.showAlert}>driver will receive an SMS on this phone number to register on Driver App</Alert>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>ID Card</Form.Label>
@@ -56,7 +67,7 @@ class AddDriver extends Component {
                             <Form.Control name="driverLicense" onChange={(event) => this.onFileChange(event)} type="file" placeholder="" />
                         </Form.Group>
                         <br />
-                        <Button className="btn btn-primary" onClick={this.submitData}>Save Driver</Button>
+                        <Button type="submit" className="btn btn-primary" onClick={this.submitData}>Save Driver</Button>
                         &nbsp;<Link className="btn btn-secondary" to={"/drivers"}>Cancel</Link>
                     </Form>
                 </Container>
